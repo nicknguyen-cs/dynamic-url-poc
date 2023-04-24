2fee37b401b739ed6193ea6670b19bdce4388337
nicknguyen-cs <nick.nguyen@contentstack.com>
2023-04-24 14:17:09 -0700

    images

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -26,7 +26,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DefaultPage />} />
           <Route
-            path="/custom-field"
+            path="/custom-field-dynamic-url"
             element={
               <Suspense>
                 <CustomFieldExtensionProvider>
--- src/containers/CustomField/CustomField.tsx
+++ src/containers/CustomField/CustomField.tsx
@@ -1,27 +1,57 @@
-import Icon from "../../assets/customfield.svg";
-import localeTexts from "../../common/locales/en-us/index";
-import parse from "html-react-parser";
+import { useCallback, useEffect, useState } from 'react'
+import { TextInput } from '@contentstack/venus-components'
+import ContentstackAppSdk from '@contentstack/app-sdk'
+import '@contentstack/venus-components/build/main.css'
 
 const CustomFieldExtension = () => {
-  return (
-    <div className="custom-field">
-      <div className="custom-field-container">
-        <div className="custom-field-icon">
-          <img src={Icon} alt="icon" />
-        </div>
-        <div className="app-component-content">
-          <h4>{localeTexts.CustomField.title}</h4>
-          <p>{parse(localeTexts.CustomField.body)}</p>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.contentstack.com/docs/developers/developer-hub/custom-field-location/">
-            {localeTexts.CustomField.button.learnMore}
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+  const [error, setError] = useState<any>(null)
+  const [app, setApp] = useState({} as any)
+  const [url, setUrl] = useState("")
+
+  const initializeApp = useCallback(async () => {
+    if (app) {
+      const customField = await app?.location?.CustomField
+      const entry = customField?.entry
+
+      setUrl(entry.getData()?.custom)
+      customField.frame.updateHeight(0)
+
+      entry.onChange((data: any) => {
+        const url = constructUrl(data)
+        setUrl(url)
+        console.log("Changing URL : ", url);
+        customField.field.setData(url);
+        entry.getField('url')?.setData(url)
+      })
+    }
+  }, [app])
+
+  const constructUrl = (data: any) => {
+    const productName = data?.title ?? ''
+    const formattedProductName = productName.split(' ').join('-').toLowerCase()
+    const category = data?.category ?? ''
+    return `/${category}/${formattedProductName}`
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line no-restricted-globals
+    if (typeof window !== 'undefined' && self === top) {
+      setError("Error")
+    } else {
+      ContentstackAppSdk.init().then((appSdk) => {
+        setApp(appSdk)
+        let customField = appSdk?.location?.CustomField
+        console.log("value" , customField?.entry?.getData()?.url, customField?.entry?.getData()?.custom);
+        initializeApp()
+      })
+    }
+  }, [initializeApp])
+
+  return error ? (
+    <h3>{error}</h3>
+  ) : (
+    <TextInput type="text" disabled value={url} />
+  )
 };
 
 export default CustomFieldExtension;
